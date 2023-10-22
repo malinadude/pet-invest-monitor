0eec66ed1c3a280751528597bbf20c6f0e06cce5
Станислав Медунов <s.medunov@notamedia.ru>
2023-10-23 01:02:54 +0300

    feature/7: Добавил FontAwesome

--- src/app/main.ts
+++ src/app/main.ts
@@ -1,12 +1,17 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faUserSecret } from '@fortawesome/free-solid-svg-icons'
+
 import App from './App.vue'
 import router from './router'
 
 import { LayoutDefault, LayoutPageNotFound } from '@/shared/components/layouts'
 
 import '@/app/assets/styles/vendors/index.scss'
+
+library.add(faUserSecret)
 
 const app = createApp(App)
 
